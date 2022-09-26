0d3af8484b87280da9d9037501747b0108ecd1c7
kushalarora <kushal18@gmail.com>
2022-09-26 12:45:33 -0400

    scripts to compute and score for corr analysis.

---
+++ text_completion/wiki/generate_for_corr_analysis.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+set -ex
+
+mkdir -p data/wiki_rankgen/corr_analysis/gpt2_xl/
+
+# Greedy
+filename=data/wiki_rankgen/corr_analysis/gpt2_xl/greedy.jsonl
+if [[ ! -f "${filename}" ]] || [[ $(($(wc -l < ${filename}) < 5000)) -eq 1 ]];
+then
+    echo "Running Greedy."
+    sbatch -t 4:00:00 ./launcher_basic.sh python text_completion/wiki/generate_from_gpt2.py --model_name_or_path gpt2-xl --output_filename ${filename} --fp16 
+fi
+
+# Beam
+for num_beams in 2 5 10 20; 
+do 
+    filename=data/wiki_rankgen/corr_analysis/gpt2_xl/beam_${num_beams}.jsonl
+    if [[ ! -f "${filename}" ]] || [[ $(($(wc -l < ${filename}) < 5000)) -eq 1 ]];
+    then
+        echo "Running Beam Search: ${num_beams}."
+        sbatch -t 48:00:00 ./launcher_basic.sh python text_completion/wiki/generate_from_gpt2.py --model_name_or_path gpt2-xl --output_filename ${filename} --num_beams ${num_beams} --batch_size $((64/${num_beams})) --fp16;
+    fi
+done
+
+for run in 1 2 3 4 5; do
+    for p in 0.25 0.5 0.75 0.9 0.95; 
+    do
+        filename="data/wiki_rankgen/corr_analysis/gpt2_xl/top_p_${p}_run_${run}.jsonl"
+        if [ ! -f "${filename}" ] || [ $(($(wc -l < "${filename}") < 5000)) -eq 1 ];
+        then
+            echo "Running Nucleus Sampling: run=${run} || top-p=${p}."
+            sbatch -t 4:00:00 ./launcher_basic.sh python text_completion/wiki/generate_from_gpt2.py --model_name_or_path gpt2-xl --output_filename ${filename}  --p ${p} --do_sample --fp16;
+        fi
+    done
+
+    for k in 5 10 30 50 100; 
+    do
+        filename="data/wiki_rankgen/corr_analysis/gpt2_xl/top_k_${k}_run_${run}.jsonl"
+        if [ ! -f "${filename}" ] || [ $(($(wc -l < "${filename}") < 5000)) -eq 1 ];
+        then
+            echo "Running Top-k Sampling: run=${run} || top-k=${k}."
+            sbatch -t 4:00:00 ./launcher_basic.sh python text_completion/wiki/generate_from_gpt2.py --model_name_or_path gpt2-xl --output_filename ${filename}  --k ${k} --do_sample --fp16;
+        fi
+    done
+
+    for tau in 0.25 0.5 0.75 0.9 0.95; 
+    do
+        filename="data/wiki_rankgen/corr_analysis/gpt2_xl/typical_p_${tau}_run_${run}.jsonl"
+        if [ ! -f "${filename}" ] || [ $(($(wc -l < "${filename}") < 5000)) -eq 1 ];
+        then
+            echo "Running Typical Sampling: run=${run} || tau=${tau}."
+            sbatch -t 4:00:00 ./launcher_basic.sh python text_completion/wiki/generate_from_gpt2.py --model_name_or_path gpt2-xl --output_filename ${filename}  --typical_p ${tau} --do_sample --fp16;
+        fi
+    done
+
+    for temp in 0.5 0.8 1.0 1.2 1.5;
+    do
+        filename="data/wiki_rankgen/corr_analysis/gpt2_xl/temp_${temp}_run_${run}.jsonl"
+        if [ ! -f "${filename}" ] || [ $(($(wc -l < "${filename}") < 5000)) -eq 1 ];
+        then
+            echo "Running Temperature Sampling: run=${run} || temp=${temp}."
+            sbatch -t 4:00:00 ./launcher_basic.sh python text_completion/wiki/generate_from_gpt2.py --model_name_or_path gpt2-xl --output_filename ${filename} --temperature ${temp} --do_sample --batch_size 16;
+        fi
+    done
+done
---
+++ text_completion/wiki/score_for_corr_analysis.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+for filename in data/wiki_rankgen/corr_analysis/gpt2_xl/*.jsonl;
+do
+    if [ -f "${filename}" ] && [ $(($(wc -l < "${filename}") > 5000)) -eq 1 ] && [ ! -f "${filename}.score" ];
+    then
+        echo ${filename}
+        sbatch -t 1:00:00 ./launcher_basic.sh python text_completion/score_generations.py --dataset ${filename}
+    fi
+done
+
