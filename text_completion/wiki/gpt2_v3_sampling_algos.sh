65d8d884d911c305d6e7aa8fb04345e738aeb033
kushalarora <kushal18@gmail.com>
2022-10-05 17:38:28 -0400

    script to generate using eags v2 and v3 variants with various sampling algorithms.

--- text_completion/wiki/gpt2_v3_sampling_algos.sh
+++ text_completion/wiki/gpt2_v3_sampling_algos.sh
@@ -1,18 +1,18 @@
 !#/bin/bash
 
 for temp in 0.8 0.9 1 1.1; do
-    sbatch -t 4:00:00 ./launcher_basic.sh python text_completion/wiki/generate_from_gpt2.py --model_name_or_path gpt2-xl --output_filename data/wiki_rankgen/generated/gpt2_xl/v3_sampling_analysis/eags_v3_temp_${temp}.jsonl --temperature ${temp} --version 3 --fp16 --entropy_aware_search
+    sbatch -t 4:00:00 ./launcher_basic.sh python text_completion/wiki/generate_from_gpt2.py --model_name_or_path gpt2-xl --output_filename data/wiki_rankgen/generated/gpt2_xl/v3_sampling_analysis/eags_v3_temp_${temp}.jsonl --temperature ${temp} --eags_version 3 --fp16 --entropy_aware_search
 done
 
 for k in 10 30 50 100; do
-    sbatch -t 4:00:00 ./launcher_basic.sh python text_completion/wiki/generate_from_gpt2.py --model_name_or_path gpt2-xl --output_filename data/wiki_rankgen/generated/gpt2_xl/v3_sampling_analysis/eags_v3_k_${k}.jsonl --k ${k} --version 3 --fp16 --entropy_aware_search
+    sbatch -t 4:00:00 ./launcher_basic.sh python text_completion/wiki/generate_from_gpt2.py --model_name_or_path gpt2-xl --output_filename data/wiki_rankgen/generated/gpt2_xl/v3_sampling_analysis/eags_v3_k_${k}.jsonl --k ${k} --eags_version 3 --fp16 --entropy_aware_search
 done
 
 for tau in 0.2 0.5 0.9 0.95; do
-    sbatch -t 4:00:00 ./launcher_basic.sh python text_completion/wiki/generate_from_gpt2.py --model_name_or_path gpt2-xl --output_filename data/wiki_rankgen/generated/gpt2_xl/v3_sampling_analysis/eags_v3_typical_${tau}.jsonl --typical_p ${tau} --version 3 --entropy_aware_search --batch_size 16
+    sbatch -t 4:00:00 ./launcher_basic.sh python text_completion/wiki/generate_from_gpt2.py --model_name_or_path gpt2-xl --output_filename data/wiki_rankgen/generated/gpt2_xl/v3_sampling_analysis/eags_v3_typical_${tau}.jsonl --typical_p ${tau} --eags_version 3 --entropy_aware_search --batch_size 16
 done
 
 
 for p in 0.2 0.5 0.9 0.95; do
-    sbatch -t 4:00:00 ./launcher_basic.sh python text_completion/wiki/generate_from_gpt2.py --model_name_or_path gpt2-xl --output_filename data/wiki_rankgen/generated/gpt2_xl/v3_sampling_analysis/eags_v3_p_${p}.jsonl --p ${p} --version 3 --entropy_aware_search --fp16
+    sbatch -t 4:00:00 ./launcher_basic.sh python text_completion/wiki/generate_from_gpt2.py --model_name_or_path gpt2-xl --output_filename data/wiki_rankgen/generated/gpt2_xl/v3_sampling_analysis/eags_v3_p_${p}.jsonl --p ${p} --eags_version 3 --entropy_aware_search --fp16
 done
