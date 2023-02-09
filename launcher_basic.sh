381522a077a896a6de96dda7617bf0f5dc410eac
kushalarora <kushal18@gmail.com>
2023-02-08 21:24:27 -0500

    added launcher_basic.sh

---
+++ launcher_basic.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-gpu=4
+#SBATCH --nodes=1
+#SBATCH --mem=48000M
+#SBATCH --mail-type=ALL,TIME_LIMIT,BEGIN,END,FAIL
+#SBATCH --mail-user=arorakus@mila.quebec
+#SBATCH --time=23:00:00
+#SBATCH --gres=gpu:3g.39gb:1
+#SBATCH -o logs/slurm-%x-%j.out
+#SBATCH -e logs/slurm-%x-%j.err
+###########################
+
+set -x
+module load cuda
+module load python/3.8 
+module load libffi
+
+export NUM_GPUS=${NUM_GPUS:=2}
+export TOKENIZERS_PARALLELISM=true
+export PYTHONPATH=.:${PYTHONPATH}
+source  ${HOME}/scratch/envs/ews2/bin/activate
+$@
+
